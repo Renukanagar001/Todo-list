c0c130d77d6907e9eb6b7e12b7f40280cc7834ba
Renuka <renukanagar.2007@gmail.com>
2026-05-27 18:20:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from "./Mycomponents/Header";
+import {Todos} from "./Mycomponents/Todos";
+import {Footer} from "./Mycomponents/Footer";
+import React, {useState} from "react";
+import {AddTodo} from "./Mycomponents/AddTodo";
+
+
 
 function App() {
+  
+  
+  // let myvariables =345;
+  const onDelete =(todo) =>{
+console.log("I am ondelete of todo",todo);
+let index = todos.indexOf(todo);
+todos.splice(index,1);
+
+setTodos(todos.filter((e)=>{  //delete the todo
+  return e!==todo;
+}));
+  }
+  const addTodo=(title,desc) =>{
+    console.log("I am adding this todo",title,desc)
+   let sno = todos[todos.length-1].sno+1;
+    const myTodo ={
+      sno: sno,
+      title:title,
+      desc:desc,
+
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+  const[todos, setTodos] = useState([
+     
+    {
+      sno: 1, 
+      title: "Go to the market",
+      desc: "You need to go to thee market to get this job Done"
+    },
+    {
+      sno: 2, 
+      title: "Go to the college",
+      desc: "You need to go to the college for  to get examination form "
+    },
+    {
+      sno: 3, 
+      title: "Go to the mall",
+      desc: "You need to go to the mall for shopping"
+    },
+  ]
+
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   { <Header title="My Todos List" searchBar={true}/> }
+   <AddTodo addTodo={addTodo}/> 
+   <Todos todos={todos} onDelete ={onDelete}/>
+   <Footer/>
+    </>
+  
   );
 }
 
 export default App;
+
+    
